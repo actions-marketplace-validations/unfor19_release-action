bbdf6123e0334d6cb069c8aa4c96cbc1166d3336
Meir Gabay <unfor19@gmail.com>
2021-06-26 19:21:29 +0300

    added more info

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /code/bargs.sh "$@"
-
+ls -lh
+echo "PWD = $PWD"
+echo "$GITHUB_WORKSPACE"
 echo "$LANG_NAME"
 echo "$LANG_VERSION"
 echo "::set-output name=lang_name::$LANG_NAME"
