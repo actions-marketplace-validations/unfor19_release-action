26e6d9d5812040cd6242d87f6e1679c02320e39d
Meir Gabay <unfor19@gmail.com>
2021-06-26 23:42:13 +0300

    changed structure

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,3 +7,9 @@ echo "$LANG_NAME"
 echo "$LANG_VERSION"
 echo "::set-output name=lang_name::$LANG_NAME"
 echo "::set-output name=lang_version::$LANG_VERSION"
+if [[ -f build.sh ]]; then
+    echo "found build.sh file"
+    bash ./build.sh
+else
+    echo "build.sh file not found"
+fi
