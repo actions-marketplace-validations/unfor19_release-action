14f6638ccbc10277fde96069d412aaaaf492cd61
Meir Gabay <unfor19@gmail.com>
2021-06-27 00:36:41 +0300

    cleanup

--- golang/1.16/src/entrypoint.sh
+++ golang/1.16/src/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 source /code/bargs.sh "$@"
-ls -lh
+
 echo "PWD = $PWD"
-echo "$GITHUB_WORKSPACE"
-echo "$LANG_NAME"
-echo "$LANG_VERSION"
-echo "::set-output name=lang_name::$LANG_NAME"
-echo "::set-output name=lang_version::$LANG_VERSION"
 if [[ $ACTION = "build" && -f build.sh ]]; then
     echo "found build.sh file"
     bash ./build.sh
