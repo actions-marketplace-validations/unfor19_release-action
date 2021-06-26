923ded58245c2f361a268d98fe2269cddbc25313
Meir Gabay <unfor19@gmail.com>
2021-06-26 23:44:06 +0300

    temporary duplication

---
+++ golang/1.16/src/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /code/bargs.sh "$@"
+ls -lh
+echo "PWD = $PWD"
+echo "$GITHUB_WORKSPACE"
+echo "$LANG_NAME"
+echo "$LANG_VERSION"
+echo "::set-output name=lang_name::$LANG_NAME"
+echo "::set-output name=lang_version::$LANG_VERSION"
+if [[ -f build.sh ]]; then
+    echo "found build.sh file"
+    bash ./build.sh
+else
+    echo "build.sh file not found"
+fi
