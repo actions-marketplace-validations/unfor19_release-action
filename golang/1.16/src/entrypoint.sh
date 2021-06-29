483fd68e90fdd88108e28ba24220e219cf7801de
Meir Gabay <unfor19@gmail.com>
2021-06-30 00:59:04 +0300

    using github workspace env var

--- golang/1.16/src/entrypoint.sh
+++ golang/1.16/src/entrypoint.sh
@@ -19,44 +19,45 @@ log_msg(){
 }
 
 log_msg "Running as $(whoami)"
-
+_SRC_DIR="${SRC_DIR:-""}"
 
 if [[ $ACTION = "build" && -f build.sh ]]; then
     log_msg "Found build.sh file"
     log_msg "Checking cache dir"
-    if [[ -d .cache-modules ]]; then
-        log_msg "Using .cache-modules"
+    if [[ -d "${GITHUB_WORKSPACE}/.cache-modules" ]]; then
+        log_msg "Using ${GITHUB_WORKSPACE}/.cache-modules"
         mkdir -p /go/pkg/mod
-        ls -lh .cache-modules
-        cp -r .cache-modules/* /go/pkg/mod/
+        ls -lh "${GITHUB_WORKSPACE}/.cache-modules"
+        cp -r "${GITHUB_WORKSPACE}/.cache-modules"/* /go/pkg/mod/
     fi
-    if [[ -d .cache-go-build ]]; then
+    if [[ -d "${GITHUB_WORKSPACE}.cache-go-build/" ]]; then
         log_msg "Cache go-build exists!"
         mkdir -p ~/.cache/go-build
-        mv .cache-go-build/* ~/.cache/go-build/
+        mv "${GITHUB_WORKSPACE}.cache-go-build/"* ~/.cache/go-build/
     fi
     log_msg "Executing build.sh script"
     bash ./build.sh
     log_msg "Finished building app"
     ls -lh
     log_msg "Caching build and modules..."
-    mkdir -p .cache-go-build
-    mv ~/.cache/go-build/* .cache-go-build/
+    mkdir -p "${GITHUB_WORKSPACE}.cache-go-build/"
+    mv ~/.cache/go-build/* "${GITHUB_WORKSPACE}.cache-go-build/"
     log_msg "Setting ownership of .cache-go-build to current user ..."
-    chown -R 1001:121 .cache-go-build
-    log_msg "Setting ownership of .cache-modules to current user ..."
-    chown -R 1001:121 .cache-modules
+    chown -R 1001:121 "${GITHUB_WORKSPACE}.cache-go-build"
+    log_msg "Setting ownership of ${GITHUB_WORKSPACE}/.cache-modules to current user ..."
+    chown -R 1001:121 "${GITHUB_WORKSPACE}/.cache-modules"
     ls -lah
 elif [[ $ACTION = "test" ]]; then
-    cd ./golang || exit 1
+    [[ "$_SRC_DIR" ]] && cd "$_SRC_DIR"
     go test -v
 elif [[ $ACTION = "dependencies" ]]; then
     log_msg "Getting dependencies ..."
+    [[ "$_SRC_DIR" ]] && cd "$_SRC_DIR"
     go mod download # -json
-    mkdir -p .cache-modules
-    mv /go/pkg/mod/* .cache-modules
-    chown -R 1001:121 .cache-modules
-    ls -lh .cache-modules
+    mkdir -p "${GITHUB_WORKSPACE}/.cache-modules"
+    mv /go/pkg/mod/* "${GITHUB_WORKSPACE}/.cache-modules/"
+    chown -R 1001:121 "${GITHUB_WORKSPACE}/.cache-modules"
+    ls -lh "${GITHUB_WORKSPACE}/.cache-modules"
 else
     error_msg "Unknown action"
 fi
